1f09322856b102aafa6e7d74099635218bf32a3a
deco <decoakbar30@gmail.com>
2025-05-27 10:52:21 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@
 import { FiAlignJustify } from "react-icons/fi"
 // component
 import Navbar from './component/navbar'
+import Section from './component/section'
 // style
 import './style/body.css'
-import './style/navbar.css'
-import './style/section.css'
-import './style/aside.css'
 import photo from './style/photo_profile.jpg'
 
 // view
@@ -14,18 +12,19 @@ const App = () => {
   return (
     <body>
         <Navbar />
-        <FiAlignJustify className='icon-menu-nav'/>
         <aside>
             <img src={photo}></img>
             <small>hello! i'm</small>
             <h2>Deco Prasetya</h2>
-            <p>Saya Deco, Full-Stack Developer dengan spesialisasi di React.js dan Node.js. Selama 1 tahun terakhir, saya membantu startup dan UMKM mengubah ide mereka menjadi aplikasi web yang powerful. Saya percaya teknologi seharusnya memecahkan masalah nyata dengan solusi yang elegan.<br></br><br></br>Di waktu luang, saya senang berkontribusi ke proyek open source dan menulis tutorial pemrograman. Terus belajar dan berbagi pengetahuan adalah prinsip saya.</p>
+            <p>Saya Deco, Full-Stack Developer junior dengan spesialisasi di React.js dan Node.js. saya seorang lulusan SMA di salah datu sekolah di Kota Madiun. sekarangsaya mau menempuh pendidikan tinggi di Politeknik Negri Madiun. Saya percaya teknologi seharusnya memecahkan masalah nyata dengan solusi yang elegan.<br></br><br></br>Di waktu luang, saya senang berkontribusi ke proyek open source dan menulis tutorial pemrograman. Terus belajar dan berbagi pengetahuan adalah prinsip saya.</p>
+            <h4>Contact Me</h4>
             <ul>
                 <a href="https://www.instagram.com/decomposisi"><button className="instagram" >INSTAGRAM</button></a>
                 <a href="https://www.tiktok.com/@decoprasertya"><button className="tiktok" >TIKTOK</button></a>
                 <a href="https://wa.me/628960312041"><button className="wa" >WHATSAPP</button></a>
             </ul>
         </aside>
+        <Section />
     </body>
   );
 }
--- src/component/section.js
+++ src/component/section.js
@@ -1,7 +1,15 @@
 const section = () => {
     return(
         <section>
-            
+            <div className="biodata">
+                <h3>BIODATA</h3>
+                <p>Nama Lengkap : Deco Akbar Prasetya</p>
+                <p>Usia : 19 Tahun</p>
+                <p>Tempat Tinggal : Kota Madiun</p>
+                <p>Pendidikan Terakhir : SMAN 1 Madiun</p>
+                <p>Hobi : Bermain Musik, Olah Raga Basket dan Membuat Program</p>
+                <text>Bonjour! dalam bahasa French yang berarti Halo! </text>
+            </div>
         </section>
     )
 }
